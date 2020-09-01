cc4013a3223430117cbab4429db94510e47fe2fd
Sourav Goswami <souravgoswami@protonmail.com>
2020-09-02 04:20:49 +0530

    Added notification manager

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,19 @@ module ApplicationHelper
 	def active?(path)
 		request.path == path ? 'active' : ''
 	end
+
+	def notify(message, button_message = 'Ok')
+		javascript_tag "notify('#{message}', '#{button_message}')"
+	end
+
+	def flash_notification_manager
+		msg = flash[:notice] || flash[:alert] || flash[:error]
+		if flash[:notice]
+			notify(msg)
+		elsif flash[:warning]
+			notify(msg)
+		elsif flash[:error]
+			notify(msg)
+		end
+	end
 end
